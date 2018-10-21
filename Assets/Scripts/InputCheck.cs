e3964fe4565f45458cd9ad35cd541f866793063a
Johannes Palvanen <palvanenj@gmail.com>
2018-10-21 18:06:04 +0300

    Initial commit

---
+++ Assets/Scripts/InputCheck.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	static async System.Threading.Tasks.Task<int> MysteryMethod(System.Collections.Generic.IEnumerable<UnityEngine.UI.Button> buttons)
+	{
+		var taskCompletionSource = new System.Threading.Tasks.TaskCompletionSource<int>();
+
+		foreach (var pair in System.Linq.Enumerable.Select(buttons, (button, index) => new {button, index}))
+			pair.button.onClick.AddListener(() => taskCompletionSource.SetResult(pair.index));
+
+		return await taskCompletionSource.Task;
+	}
+}
