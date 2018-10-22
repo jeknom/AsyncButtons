735d259e0b9e0270f163f1f06f61d5e98cd09e46
Johannes Palvanen <palvanenj@gmail.com>
2018-10-22 18:17:12 +0300

    Fourth Commit

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -3,25 +3,63 @@ using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Diagnostics;
 
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Main : MonoBehaviour {
 	
-	public Button[] Buttons;
-	private Score _score = new Score();
-	private Stopwatch _stopwatch = new Stopwatch();
+	public List<Button> Buttons;
+	public Text[] Texts;
+	private bool[] IsActive = new bool[4];
+	private int[] Score = new int[4];
+
+	private System.Diagnostics.Stopwatch _stopwatch = new System.Diagnostics.Stopwatch();
 	
 	async void Start () 
 	{
-		_stopwatch.Start();
 		var random = new System.Random();
-		var randomizedTime = random.Next(1000, 10000);
-		// var playerWhoTapped = GetButtonTap(Buttons);
+		foreach (var status in IsActive)
+			status.Equals(true);
+
+		while(true)
+		{
+			var timeUntilTapAllowed = random.Next(3, 6);
+			foreach (var button in Buttons)
+				button.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+
+			_stopwatch.Start();
+			StartCoroutine(ToggleButtonColorWithDelay(Buttons, timeUntilTapAllowed));
+			
+			var playerWhoTapped = await GetButtonTap(Buttons);
+			_stopwatch.Stop();
+
+			if (_stopwatch.ElapsedMilliseconds >= timeUntilTapAllowed * 1000)
+			{
+				Score[playerWhoTapped] += 1;
+				for (var i = 0; i < 4; i++)
+				{
+					Texts[i].text = string.Format("Player {0}: {1}", i + 1, Score[i]); 
+				}
+			}
+			else
+			{
+				Buttons[playerWhoTapped].GetComponent<Image>().color = new Color32(120, 120, 120, 255);
+				IsActive[playerWhoTapped] = false;
+			}
+
+			_stopwatch.Reset();
+		}
 	}
 
+	IEnumerator ToggleButtonColorWithDelay(List<Button> buttons, float delayInSeconds)
+	{
+		yield return new WaitForSeconds(delayInSeconds);
+		foreach (var button in buttons)
+		{
+			button.GetComponent<Image>().color = new Color32(0, 255, 0, 255);
+		}
+	}
 	static async Task<int> GetButtonTap(IEnumerable<Button> buttons)
 	{
 		var taskCompletionSource = new TaskCompletionSource<int>();
@@ -30,6 +68,11 @@ public class Main : MonoBehaviour {
 			pair.button.onClick.AddListener(() =>
 				taskCompletionSource.SetResult(pair.index));
 		
-		return await taskCompletionSource.Task;
+		var result = await taskCompletionSource.Task;
+
+		foreach (var button in buttons)
+			button.onClick.RemoveAllListeners();
+
+		return result;
 	}
 }
