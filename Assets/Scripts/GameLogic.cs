007f3faa74aa356f65a08d706ff82dbdd58f8820
Johannes Palvanen <palvanenj@gmail.com>
2018-10-29 17:42:02 +0200

    Compressed

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -1,45 +1,37 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public static class Players
-{
-	public static int[] Score = new int[4];
-}
-
 public class GameLogic : MonoBehaviour 
 {
+	private static int[] Score = new int[4];
 	public Button[] Buttons;
 	public Text[] Texts;
-
-	private System.Random random = new System.Random();
 
 	async void Start () 
 	{
 		for (var i = 0; i < Texts.Length; i++)
-			Texts[i].text = Players.Score[i].ToString();
+			Texts[i].text = Score[i].ToString();
 
-		var prohibitedTap = GetTap(Buttons);
-		var allowedTap = ToggleButtonsAfterDelay(Buttons, TimeSpan.FromSeconds(3));
+		var randomDelay = Task.Delay(Random.Range(2000, 3000));
+		var getTap = GetTap(Buttons);
 
-		var completedTask = await Task.WhenAny(prohibitedTap, allowedTap);
-		var playerIndex = completedTask.Result;
+		var completedTask = await Task.WhenAny(randomDelay, getTap);
 
-		if (completedTask == prohibitedTap)
+		if (completedTask == getTap)
+			for (var i = 0; i < Score.Length; i++)
+				Score[i] = (i != getTap.Result) ? Score[i] + 1 : Score[i] - 1;
+		
+		else if (completedTask == randomDelay)
 		{
-			Players.Score[playerIndex]--;
-			for (var i = 0; i < Players.Score.Length; i++)
-				if (Array.IndexOf(Players.Score, Players.Score[i]) != playerIndex)
-					Players.Score[i]++;
+			foreach (var button in Buttons)
+				button.GetComponent<Image>().color = new Color32(0, 255, 0, 255);
+			Score[await GetTap(Buttons)]++;
 		}
-		else if (completedTask == allowedTap)
-			Players.Score[playerIndex]++;
-
+		
 		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 	}
 
@@ -51,18 +43,5 @@ public class GameLogic : MonoBehaviour
 			pair.button.onClick.AddListener(() => taskCompletionSource.SetResult(pair.index));
 
 		return await taskCompletionSource.Task;
-	}
-
-	static async Task<int> ToggleButtonsAfterDelay(Button[] buttons, TimeSpan delay)
-	{
-		await Task.Delay(delay);
-		
-		foreach (var button in buttons)
-		{
-			button.onClick.RemoveAllListeners();
-			button.GetComponent<Image>().color = new Color(0, 255, 0, 255);
-		}
-		
-		return await GetTap(buttons);
 	}
 }
